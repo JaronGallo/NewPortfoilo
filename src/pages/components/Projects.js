0c0248fa41875d3d262843bdee4a4301976a3b77
Jaron Gallo <you@example.com>
2018-03-15 21:13:52 -0600

    added new project

--- src/pages/components/Projects.js
+++ src/pages/components/Projects.js
@@ -101,7 +101,7 @@ export default class Projects extends Component{
                                     <div className="content">
                                         <a className="header">MongoDb</a>
                                         <div className="meta">
-                                            <span className="date">Using API</span>
+                                            <span className="date">Using APIs</span>
                                         </div>
                                     </div>
                                 </div>
