7a7d88d5664e1a8addfa6574f104d7cccf9ab7ee
Jaron Gallo <you@example.com>
2018-03-29 16:19:18 -0600

    testing bug fixes

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res)=>{
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 80);
 
 console.log("works");
