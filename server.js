2b4eeb79e6dfa8223bd3fa13577a45812ce052a9
Jaron Gallo <you@example.com>
2018-05-03 21:56:03 -0600

    New design

--- server.js
+++ server.js
@@ -57,8 +57,9 @@ app.post('/api/form', (req,res) => {
 
         
         app.get('*', function (request, response){
-          response.sendFile(path.resolve(__dirname, '../NewPortfoilio/build', 'index.html'))
-      })
+          const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
+      });
        
         // Dont forget to change back to 80
 app.listen(process.env.PORT || 80);
