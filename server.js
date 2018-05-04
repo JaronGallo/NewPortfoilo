a201ac546b77e2a43992cfd14f0c8d29814df477
Jaron Gallo <you@example.com>
2018-05-03 20:06:04 -0600

    New design

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/",function(req,res){
-    res.sendfile(path.join(__dirname, '../public', 'index.html'));
+    res.sendfile(path.join(__dirname, '../build/public', 'index.html'));
   //res.send("test");
 });
         
