658164b9bc15a31ecf4ea3d50d0909d9061d9f74
Jaron Gallo <you@example.com>
2018-05-03 19:59:17 -0600

    New design

--- server.js
+++ server.js
@@ -5,19 +5,17 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(path.join(__dirname, 'build')));
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-
+app.use(express.static(path.join(__dirname, 'build')));
 
         app.get('*', (req, res)=>{
           res.sendFile(path.join(__dirname, '../build/index.html'));
         });
-        app.get('/contact', (req, res)=>{
-          res.sendFile(path.join(__dirname, '../build/index.html'));
-        });
+        
 
         app.post('/api/form', (req,res) => {
           nodemailer.createTestAccount((err, account) => {
