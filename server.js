77c23202dd772e5efe4a8189de079054020088d0
Jaron Gallo <you@example.com>
2018-05-03 22:36:32 -0600

    New design

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.post('/api/form', (req,res) => {
 
    
       app.get('*', (req, res)=>{
-        res.sendFile(path.join(__dirname, '../NewPortfolio/build/index.html'));
+        res.sendFile(path.join(__dirname, './build/index.html'));
       })
 
 
