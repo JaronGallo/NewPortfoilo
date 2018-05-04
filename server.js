dc8e89a60a4fd84c0b262d5c22c9f1500228215c
Jaron Gallo <you@example.com>
2018-05-03 22:32:04 -0600

    New design

--- server.js
+++ server.js
@@ -55,11 +55,12 @@ app.post('/api/form', (req,res) => {
   })
 })
 
-        
-app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile(index);
-});
+     app.get('*', function (req, res) {
+      app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, '../build/index.html'));
+      })
+});   
+
        
         // Dont forget to change back to 80
 app.listen(process.env.PORT || 80);
